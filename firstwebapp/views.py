a2ca8d45908b5f20be566fecbdec0c83eb702c96
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-04-10 23:53:33 +0530

    Add a new return statement inside the index() function and call the render() function in it.

--- firstwebapp/views.py
+++ firstwebapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     # return HttpResponse("Hey, this is my first web app.")
-
+    return render(request, "firstwebapp/index.html")
 
 def another(request):
     return HttpResponse("Hello, User!")
