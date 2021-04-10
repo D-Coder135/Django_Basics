72490b15b7bf20c430d278c11dad346297e6a296
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-04-10 23:41:55 +0530

    Add a new path() for the greet() function inside the urls.py

--- firstwebapp/urls.py
+++ firstwebapp/urls.py
@@ -4,6 +4,7 @@ from firstwebapp import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
     path("another", views.another, name="Another"),
     path("anotherfunction", views.anotherfunction, name="AnotherFunction")
 ]
