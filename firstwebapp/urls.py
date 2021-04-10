b88d005bc779a1a7eb2616fe2995b4b16ebdc01f
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-04-10 15:04:51 +0530

    add a new path for the another() function inside the urls.py

--- firstwebapp/urls.py
+++ firstwebapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from firstwebapp import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("another", views.another, name="Another")
 ]
