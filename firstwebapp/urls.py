a2a0ac736a36a5e546b7ae6de6b889af6a2f1ee0
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-04-10 15:09:30 +0530

    Add a new path for the function anotherfunction() inside the urls.py

--- firstwebapp/urls.py
+++ firstwebapp/urls.py
@@ -5,4 +5,5 @@ from firstwebapp import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("another", views.another, name="Another")
+    path("anotherfucntion", views.anotherfunction, name="AnotherFunction")
 ]
