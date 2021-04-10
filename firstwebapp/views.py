97e9e9b5e58e605736c36f4ee737e30169234827
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-04-10 15:03:35 +0530

    Reformat!

--- firstwebapp/views.py
+++ firstwebapp/views.py
@@ -5,5 +5,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hey, this is my first web app.")
 
+
 def another(request):
     return HttpResponse("Hello, User!")
