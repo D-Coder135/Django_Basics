8f0d7b65829847b9ca59ba87add340badead3fd5
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-04-11 13:40:51 +0530

    Add path for the new application newyear inside the urls.py

--- First/urls.py
+++ First/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('firstwebapp/', include("firstwebapp.urls"))
+    path('firstwebapp/', include("firstwebapp.urls")),
+    path('newyear/', include("newyear.urls"))
 ]
