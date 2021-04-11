6d21993ca6828b703912d93564aff9d666d3c1b6
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-04-11 13:54:54 +0530

    Add a return statement inside the index() function and gave reference for the index.html file in it

--- newyear/views.py
+++ newyear/views.py
@@ -4,3 +4,6 @@ import datetime
 
 def index(request):
     now = datetime.datetime.now()
+    return render(request, "newyear/index.html", {
+        "newyear": now.month == 1 and now.day == 1
+    })
