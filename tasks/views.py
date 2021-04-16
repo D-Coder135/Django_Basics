8450dbcec6d9b5be39019dc11812eb27e4f76a31
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-17 00:24:16 +0530

    Add return statement for index() function and call render function inside it

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 tasks = ["Wake Up!", "Code", "Eat", "Sleep!"]
+
+
 # Create your views here.
 def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
