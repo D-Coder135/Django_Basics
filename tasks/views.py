b17440c93d91a257bac38d5853e54e1e5d5c0ad6
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-19 11:40:43 +0530

    return an empty list of task if there isn't any tasks present already for a particular session

--- tasks/views.py
+++ tasks/views.py
@@ -14,7 +14,7 @@ class NewTaskForm(forms.Form):
 # Create your views here.
 def index(request):
     if "tasks" not in request.session:
-
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
         "tasks": tasks
     })
